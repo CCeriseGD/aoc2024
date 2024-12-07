2539d228848859413dab5f6647d2bc236bf4fc2e
CCeriseGD <ccerisegd@gmail.com>
2024-12-07 11:33:22 +0100

    better day 7

---
+++ optimized/07/main.lua
@@ -0,0 +1,47 @@
+local total1 = 0
+local total2 = 0
+for line in love.filesystem.lines("input") do
+    local testvalue, values = line:match("(%d+): (.+)")
+    testvalue = tonumber(testvalue)
+    local totals = {}
+    for m in values:gmatch("%d+") do
+        local n = tonumber(m)
+        local t = {}
+        if #totals == 0 then
+            table.insert(t, {n, false})
+        else
+            for i = 1, #totals do
+                local vadd = totals[i][1] + n
+                local vmul = totals[i][1] * n
+                local vconcat = tonumber(totals[i][1] .. n)
+                if vadd <= testvalue then
+                    table.insert(t, {vadd, totals[i][2]})
+                end
+                if vmul <= testvalue then
+                    table.insert(t, {vmul, totals[i][2]})
+                end
+                if vconcat <= testvalue then
+                    table.insert(t, {vconcat, true})
+                end
+            end
+        end
+        totals = t
+    end
+    for i = 1, #totals do
+        if totals[i][1] == testvalue and not totals[i][2] then
+            total1 = total1 + testvalue
+            break
+        end
+    end
+    for i = 1, #totals do
+        if totals[i][1] == testvalue then
+            total2 = total2 + testvalue
+            break
+        end
+    end
+end
+print("part 1:", string.format("%d", total1))
+print("part 2:", string.format("%d", total2))
+
+
+
